158b0a3864a402bb0dc975fa04daa3748df523c7
amrishtembe1998 <amrishtembe1998@gmail.com>
2026-02-10 08:48:31 +0530

    feat(firebase): deployed to firebase

--- frontend/src/utilities/firebase.js
+++ frontend/src/utilities/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 import { FIREBASE_API_KEY } from "../constants";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,16 +9,15 @@ import { FIREBASE_API_KEY } from "../constants";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
-  authDomain: "gpt-project-be1c5.firebaseapp.com",
-  projectId: "gpt-project-be1c5",
-  storageBucket: "gpt-project-be1c5.firebasestorage.app",
-  messagingSenderId: "806159363582",
-  appId: "1:806159363582:web:d6d03982ae140103ab64af",
-  measurementId: "G-F1VW92K2JJ",
+  authDomain: "ai-powered-expense-track-91958.firebaseapp.com",
+  projectId: "ai-powered-expense-track-91958",
+  storageBucket: "ai-powered-expense-track-91958.firebasestorage.app",
+  messagingSenderId: "363643321904",
+  appId: "1:363643321904:web:0106521c9596c1ace4b7f5",
+  measurementId: "G-1WL5W3Z60E"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
